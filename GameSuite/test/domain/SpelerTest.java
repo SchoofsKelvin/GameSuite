cc7bcf81372f73849b8d8f05cacd56f26f45a9cc
Mieke Kemme <mieke.kemme@ucll.be>
2016-04-23 12:34:10 +0200

    Refactored testmethod names

--- GameSuite/test/domain/SpelerTest.java
+++ GameSuite/test/domain/SpelerTest.java
@@ -9,86 +9,79 @@ public class SpelerTest {
 	
 	private String naam;
 	private String anderenaam;
-	private int scoreToAdd;
-	private int wrongScoreToAdd;
+	private int positiveScore;
+	private int negativeScore;
 	private Speler speler;
 
 	@Before
 	public void setUp() throws Exception {
 		naam = "Lars";
 		anderenaam = "Lies";
-		//TODO Remove? scoreToAdd = 5;
-		//TODO Remove? wrongScoreToAdd = -5;
+		positiveScore = 5;
+		negativeScore = -5;
 		speler = new Speler(naam);
 	}
 
 	@Test
-	public void test_Speler_wordt_aangemaakt_met_gegeven_naam() {
+	public void Speler_moet_speler_aanmaken_met_gegeven_naam() {
 		speler = new Speler(naam);
 		assertEquals(naam, speler.getNaam());
 		assertEquals(0, speler.getScore());
 	}
 	
 	@Test (expected = DomainException.class)
-	public void test_Speler_Gooit_exception_als_naam_null() {
+	public void Speler_moet_exception_gooien_als_naam_null() {
 		speler = new Speler(null);
 	}
 	
 	@Test (expected = DomainException.class)
-	public void test_Speler_Gooit_exception_als_naam_lege_string() {
+	public void Speler_moet_exception_gooien_als_naam_lege_string() {
 		speler = new Speler("");
 	}
 	
 	@Test
-	public void test_equals_geeft_true_als_naam_gelijk_is(){
+	public void equals_moet_true_teruggeven_als_naam_en_score_gelijk_zijn(){
+		speler.addToScore(positiveScore);
 		Speler andereSpeler = new Speler(naam);
+		andereSpeler.addToScore(positiveScore);
 		
 		assertTrue(speler.equals(andereSpeler));
 	}
 	
 	@Test
-	public void test_equals_geeft_false_voor_parameter_null(){
+	public void equals_moet_false_teruggeven_als_parameter_null(){
 		assertFalse(speler.equals(null));
 	}
 	
 	@Test
-	public void test_equals_geeft_false_voor_speler_met_andere_naam(){
+	public void equals_moet_false_teruggeven_als_speler_een_andere_naam_heeft(){
 		Speler andereSpeler = new Speler(anderenaam);
 		assertFalse(speler.equals(andereSpeler));
 	}
 	
 	@Test
-	public void test_equals_geeft_true_als_naam_en_score_gelijk_zijn(){
-		speler.addToScore(scoreToAdd);
+	public void equals_moet_false_teruggeven_als_speler_aan_andere_score_heeft(){
 		Speler andereSpeler = new Speler(naam);
-		andereSpeler.addToScore(scoreToAdd);
-		
-		assertTrue(speler.equals(andereSpeler));
-	}
-	
-	@Test
-	public void test_equals_geeft_false_voor_speler_met_andere_score(){
-		Speler andereSpeler = new Speler(naam);
-		andereSpeler.addToScore(scoreToAdd);
+		andereSpeler.addToScore(positiveScore);
 		assertFalse(speler.equals(andereSpeler));
 	}
 	
 	@Test
-	public void test_addToScore_voegt_positieve_score_toe_aan_bestaande_score(){
-		speler.addToScore(scoreToAdd);
-		assertEquals(scoreToAdd, speler.getScore());
+	public void addToScore_moet_gegeven_score_toevoegen_aan_bestaande_score(){
+		speler.addToScore(positiveScore);
+		assertEquals(positiveScore, speler.getScore());
 	}
 	
 	@Test
-	public void test_addtoScore_voegt_score_correct_toe_voor_negatieve_score_kleiner_dan_huidige_score(){
-		speler.addToScore(scoreToAdd);
-		speler.addToScore(scoreToAdd);
-		speler.addToScore(wrongScoreToAdd);
-		assertEquals(scoreToAdd, speler.getScore());
+	public void addtoScore_mag_negatieve_score_toevoegen_als_de_resulterende_score_niet_negatief_wordt(){
+		speler.addToScore(positiveScore);
+		speler.addToScore(positiveScore);
+		speler.addToScore(negativeScore);
+		assertEquals(positiveScore, speler.getScore());
 	}
 	
 	@Test (expected = DomainException.class)
-	public void test_addToScore_Gooit_exception_als_resulterende_score_negatief(){
-		speler.addToScore(wrongScoreToAdd);
+	public void addToScore_moet_exception_gooien_als_resulterende_score_negatief_wordt(){
+		speler.addToScore(negativeScore);
 	}
 }
