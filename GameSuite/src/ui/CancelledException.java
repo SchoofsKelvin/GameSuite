8fd1c845d9b85c1c37987a8d3dd7bb8947e7c112
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-04-29 17:16:41 +0200

    CancelledException & verbetering vraagStraal

---
+++ GameSuite/src/ui/CancelledException.java
@@ -0,0 +1,7 @@
+package ui;
+
+public class CancelledException extends Exception {
+
+	private static final long serialVersionUID = 8437436962723972611L;
+
+}
--- GameSuite/src/ui/PictionaryUi.java
+++ GameSuite/src/ui/PictionaryUi.java
@@ -2,19 +2,17 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import domain.DomainException;
-
 public class PictionaryUi {
 	
 	private void toonError(String message) {
 		JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
 	}
 
-	private int vraagStraal() {
+	private int vraagStraal() throws CancelledException {
 		while (true) {
 			String straal = JOptionPane.showInputDialog("Straal?");
 			if (straal == null) {
-				throw new DomainException("Geanuleerd");
+				throw new CancelledException();
 			}
 			try {
 				return Integer.parseUnsignedInt(straal);
