7ccb31501a644d02cd88ac02bd28c58de2472173
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-04-29 17:20:32 +0200

    Merge en goede naam

--- GameSuite/src/ui/PictionaryUi.java
+++ GameSuite/src/ui/PictionaryUi.java
@@ -1,9 +1,40 @@
 package ui;
 
-import javax.swing.JOptionPane;
+import domain.Punt;
+
+import javax.swing.*;
 
 public class PictionaryUi {
-	
+
+    private Speler speler;
+
+    public PictionaryUi(Speler speler) {
+        this.speler = speler;
+    }
+
+    public void showMenu() {
+        int x = getIntegerUsingDialog("x coordinaat van het punt:");
+        int y = getIntegerUsingDialog("y coordinaat van het punt:");
+        Punt punt = new Punt(x, y);
+        JOptionPane.showMessageDialog(null, "U heeft een correct punt aangemaakt: " + punt.toString());
+    }
+
+    private int getIntegerUsingDialog(String message) throws CancelledException {
+        while (true) {
+            try {
+            	String input = JOptionPane.showInputDialog(message);
+            	if (input == null) {
+            		throw new CancelledException();
+            	}
+                return Integer.parseUnsignedInt(input);
+            } catch (NumberFormatException e) {
+                toonError("Invalid number!");
+            } catch (Exception e) {
+                toonError("There was a problem!\n" + e.getClass() + ": " + e.getMessage());
+            }
+        }
+    }
+    
 	private void toonError(String message) {
 		JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
 	}
@@ -21,4 +52,5 @@ public class PictionaryUi {
 			}
 		}
 	}
+
 }
