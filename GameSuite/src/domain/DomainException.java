2d759235674ced5473305723d76ad59e73ea6772
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-04-29 17:17:22 +0200

    serialVersionUID voor DomainException

--- GameSuite/src/domain/DomainException.java
+++ GameSuite/src/domain/DomainException.java
@@ -1,6 +1,9 @@
 package domain;
 
 public class DomainException extends RuntimeException{
+
+	private static final long serialVersionUID = 3471963563294966388L;
+
 	public DomainException(){
 		super();
 		}
