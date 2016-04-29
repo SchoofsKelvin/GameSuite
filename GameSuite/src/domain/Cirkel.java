725e0b439f74de33448fa763e670af6b072c59c0
sapkotasuren <sapkotasuren@hotmail.com>
2016-04-29 17:06:34 +0200

    Cirkel en CirkelTest toegevoegd

---
+++ GameSuite/src/domain/Cirkel.java
@@ -0,0 +1,45 @@
+package domain;
+
+public class Cirkel {
+
+	private Punt middelpunt;
+	private int radius;
+	
+	public Cirkel(Punt middelPunt,int radius){
+		setMiddelpunt(middelpunt);
+		setRadius(radius);
+	}
+
+	public Punt getMiddelpunt() {
+		return middelpunt;
+	}
+
+	public void setMiddelpunt(Punt middelpunt) {
+		if(middelpunt==null){
+			throw new DomainException("Ongeldige middelpunt");
+		}
+		this.middelpunt = middelpunt;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		if(radius<=0){
+			throw new DomainException("Ongeldige radius moet groter zijn dan 0");
+		}
+		this.radius = radius;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		return obj != null && obj.getClass().equals(getClass())
+				&& ((Cirkel) obj).getMiddelpunt().equals(middelpunt)
+				&& ((Cirkel) obj).getRadius() == this.getRadius;
+	}
+	@Override
+	public String toString() {
+		return "Cirkel: " + middelpunt + " - straal: " + 20;
+	}
+}
---
+++ GameSuite/test/domain/CirkelTest.java
@@ -0,0 +1,62 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CirkelTest {
+	private final static Punt middelPunt = new Punt(200, 200);
+	private final static int radius = 20;
+
+	@Test
+	public void geldige_Cirkel_aanmaken() {
+		Cirkel cirkel = new Cirkel(middelPunt, radius);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Cirkel_met_middelpunt_null_gooit_exception() {
+		Cirkel cirkel = new Cirkel(null, radius);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Cirke_met_negatieve_radius_gooit_exception() {
+		Cirkel cirkel = new Cirkel(middelPunt, -5);
+	}
+
+	@Test(expected = DomainException.class)
+	public void Cirke_met_radius_0_gooit_exception() {
+		Cirkel cirkel = new Cirkel(middelPunt, 0);
+	}
+
+	@Test
+	public void Cirkels_zijn_gelijk_als_ze_alle_instantie_variabelen_gelijk_hebben
+
+	{
+		Cirkel cirkelA = new Cirkel(middelPunt, radius);
+		Cirkel cirkelB = new Cirkel(middelPunt, radius);
+		assertEquals(cirkelA, cirkelB);
+	}
+	@Test
+	public void Cirkels_zijn_verschillend_als_tweede_cirkel_null_is(){
+		Cirkel cirkelA = new Cirkel(middelPunt, radius);
+		assertFalse(cirkelA.equals(null));
+	}
+	@Test
+	public void Cirkels_zijn_verschillend_met_verschillend_middelpunt(){
+	{
+		Cirkel cirkelA = new Cirkel(middelPunt, radius);
+		Cirkel cirkelB = new Cirkel(new Punt(100,100), radius);
+		assertFalse(cirkelA.equals(cirkelB));
+	}
+	@Test
+	public void Cirkels_zijn_verschillend_met_verschillende_straal(){
+	{
+		Cirkel cirkelA = new Cirkel(middelPunt, radius);
+		Cirkel cirkelB = new Cirkel(middelPunt, radius+1);
+		assertFalse(cirkelA.equals(cirkelB));
+	}
+	
+	
+	
+}
