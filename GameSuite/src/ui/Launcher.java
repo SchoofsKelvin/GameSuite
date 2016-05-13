6f1fdee4935da09a2883652a5111de58bd932d71
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-05-13 17:02:10 +0200

    Asks for creation of shape after player has been made now

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -2,13 +2,16 @@ package ui;
 
 import javax.swing.JOptionPane;
 
-import domain.Speler;
+import domain.*;
 
 public class Launcher {
 
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
+		PictionaryUi ui = new PictionaryUi(speler);
+
+		ui.getVormByDialog(); //Demo
 
 		JOptionPane.showMessageDialog(null,
 				"... heeft als score " + speler.getScore());
