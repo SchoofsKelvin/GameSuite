3d3afd798928608934e42c6cde4bef09ca912b0e
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-05-13 17:35:58 +0200

    Migrated some code to PictionaryUI

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -11,13 +11,7 @@ public class Launcher {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
 		PictionaryUi ui = new PictionaryUi(speler);
-
-		Vorm vorm = ui.getVormByDialog();
-		JOptionPane.showMessageDialog(null, vorm.toString());
-		//TODO: try/catch
-		
-		JOptionPane.showMessageDialog(null,
-				"... heeft als score " + speler.getScore());
+		ui.start();
 	}
 	
 	
