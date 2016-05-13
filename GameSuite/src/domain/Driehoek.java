ba2ff29179bfe006e09a7cf068403d6d1dc40b3d
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-05-13 16:49:16 +0200

    Driehoek setHoekPunten

--- GameSuite/src/domain/Driehoek.java
+++ GameSuite/src/domain/Driehoek.java
@@ -8,10 +8,15 @@ public class Driehoek {
 		setHoekPunten(hoekPunt1, hoekPunt2, hoekPunt3);
 	}
 
-	private void setHoekPunten(Punt hoekPunt1, Punt hoekPunt2, Punt hoekPunt3) {
-		this.hoekpunt1 = hoekPunt1;
-		this.hoekpunt2 = hoekPunt2;
-		this.hoekpunt3 = hoekPunt3;
+	private void setHoekPunten(Punt punt1, Punt punt2, Punt punt3) {
+		if (punt1 == null || punt2 == null || punt3 == null) {
+			throw new DomainException("Hoekpunten mogen niet null zijn");
+		} else if ((punt2.getX()-punt1.getX())*(punt3.getY()-punt1.getY()) == ((punt3.getX()-punt1.getX())*(punt2.getY()-punt1.getY()))) {
+			throw new DomainException("Hoekpunten mogen niet op 1 lijn liggen");
+		}
+		this.hoekpunt1 = punt1;
+		this.hoekpunt2 = punt2;
+		this.hoekpunt3 = punt3;
 	}
 
 	public Punt getHoekPunt1() {
@@ -28,7 +33,6 @@ public class Driehoek {
 	
 	@Override
 	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
 		if (this == obj) return true;
 		if (obj == null || getClass() != obj.getClass()) return false;
 
