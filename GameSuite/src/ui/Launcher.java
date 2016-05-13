6fce883d9ef027d08b25165e8354e00726648129
r0624401 <jens_wouters@live.be>
2016-05-13 16:18:50 +0200

    Changed parseUnsignedInt to parseInt

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -9,9 +9,12 @@ public class Launcher {
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
 		Speler speler = new Speler(naam);
-
+		PictionaryUi ui = new PictionaryUi(speler);
+		
 		JOptionPane.showMessageDialog(null,
 				"... heeft als score " + speler.getScore());
 	}
+	
+	
 
 }
