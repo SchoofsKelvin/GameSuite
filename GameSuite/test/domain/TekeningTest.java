cf65b7829d2c75f6c832d2d8641ea09a3e1412eb
Secret <nobody@example.com>
2016-05-18 15:12:51 +0200

    TekeningTest verbeteringen

--- GameSuite/test/domain/TekeningTest.java
+++ GameSuite/test/domain/TekeningTest.java
@@ -8,9 +8,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TekeningTest {
-	
-	// TODO: verwijderVorm(null) exception
-	// TODO: deftige namen
 
 	private Tekening t;
 
@@ -24,18 +21,15 @@ public class TekeningTest {
 		new Tekening(null);
 	}
 
+	/* VOEGTOE */
+	
 	@Test(expected = DomainException.class)
-	public void VoegToe_gooit_exception_als_type_null() {
+	public void voegToe_gooit_exception_als_type_null() {
 		t.voegToe(null);
 	}
 
-	@Test(expected = DomainException.class)
-	public void getVorm_gooit_exception_als_index_negatief_is() {
-		t.getVorm(-1);
-	}
-
 	@Test
-	public void je_moet_met_de_voeg_toe_methode_de_vorm_kunnen_toevoegen() {
+	public void voegToe_werkt() {
 		Tekening t = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		Vorm c = new Cirkel(new Punt(50, 50), 10);
@@ -46,26 +40,44 @@ public class TekeningTest {
 		assertEquals(t.getVorm(1), c);
 	}
 
+	@Test (expected = DomainException.class)
+	public void voegToe_binnen_bounds_left(){
+		Tekening t = new Tekening("");
+		Vorm rechthoek = new Rechthoek(new Punt(-2,2), 50, 50);
+		t.voegToe(rechthoek);
+	}
+
+	@Test (expected = DomainException.class)
+	public void voegToe_binnen_bounds_right(){
+		Tekening t = new Tekening("");
+		Vorm rechthoek = new Rechthoek(new Punt(2,2), 500000, 50);
+		t.voegToe(rechthoek);
+	}
+
+	@Test (expected = DomainException.class)
+	public void voegToe_binnen_bounds_up(){
+		Tekening t = new Tekening("");
+		Vorm rechthoek = new Rechthoek(new Punt(2,-2), 50, 50);
+		t.voegToe(rechthoek);
+	}
+
+	@Test (expected = DomainException.class)
+	public void voegToe_binnen_bounds_down(){
+		Tekening t = new Tekening("");
+		Vorm rechthoek = new Rechthoek(new Punt(2,2), 50, 500000);
+		t.voegToe(rechthoek);
+	}
+
+	/* GETVORM */
+	
+	@Test(expected = DomainException.class)
+	public void getVorm_gooit_exception_als_index_negatief_is() {
+		t.getVorm(-1);
+	}
+
 	@Test(expected = DomainException.class)
 	public void getVorm_gooit_exception_als_index_groter_dan_size_is() {
 		t.getVorm(t.getAantalVormen()+2);
-	}
-
-	@Test
-	public void getAantalVormen_geeft_juiste_waarde_terug() {
-		Tekening t = new Tekening("test");
-		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
-		Vorm c = new Cirkel(new Punt(50, 50), 10);
-		t.voegToe(v);
-		t.voegToe(c);
-		assertEquals(2, t.getAantalVormen());
-	}
-
-	@Test
-	public void bevat_geeft_vals_als_vorm_niet_in_tekening_zit() {
-		Tekening tekening = new Tekening("test");
-		Vorm v = new Rechthoek(new Punt(20, 80), 10, 10);
-		assertFalse(tekening.bevat(v));
 	}
 
 	@Test
@@ -77,9 +89,11 @@ public class TekeningTest {
 		t.voegToe(c);
 		assertEquals(v, t.getVorm(0));
 	}
+	
+	/* VERWIJDER */
 
 	@Test
-	public void verwijderd_moet_vorm_verwijderen() {
+	public void verwijder_moet_vorm_verwijderen() {
 		Tekening tekening = new Tekening("test");
 
 		Vorm driehoek = new Driehoek(new Punt(10, 5), new Punt(5, 10), new Punt(3, 8));
@@ -93,42 +107,40 @@ public class TekeningTest {
 		assertFalse(tekening.bevat(rechthoek));
 	}
 
+	@Test (expected = DomainException.class)
+	public void verwijder_met_null_als_parameter_gooit_exception() {
+		t.verwijder(null);
+	}
+
+	/* ANDEREN */
+	
 	@Test
-	public void Tekening_equals() {
+	public void getAantalVormen_werkt() {
+		Tekening t = new Tekening("test");
+		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
+		Vorm c = new Cirkel(new Punt(50, 50), 10);
+		t.voegToe(v);
+		t.voegToe(c);
+		assertEquals(2, t.getAantalVormen());
+	}
+
+	@Test
+	public void bevat_werkt() {
+		Tekening tekening = new Tekening("test");
+		Vorm v = new Rechthoek(new Punt(20, 80), 10, 10);
+		assertFalse(tekening.bevat(v));
+		tekening.voegToe(v);
+		assertTrue(tekening.bevat(v));
+	}
+
+	@Test
+	public void Tekening_equals_werkt() {
 		Tekening t = new Tekening("jef");
 		Tekening a = new Tekening("Bart");
 		Tekening b = new Tekening("john");
 		Tekening c = new Tekening("Bart");
 		assertTrue(c.equals(a));
 		assertFalse(t.equals(b));
-	}
-
-	@Test (expected = DomainException.class)
-	public void Tekening_vorm_binnen_bounds_left(){
-		Tekening t = new Tekening("");
-		Vorm rechthoek = new Rechthoek(new Punt(-2,2), 50, 50);
-		t.voegToe(rechthoek);
-	}
-
-	@Test (expected = DomainException.class)
-	public void Tekening_vorm_binnen_bounds_right(){
-		Tekening t = new Tekening("");
-		Vorm rechthoek = new Rechthoek(new Punt(2,2), 500000, 50);
-		t.voegToe(rechthoek);
-	}
-
-	@Test (expected = DomainException.class)
-	public void Tekening_vorm_binnen_bounds_up(){
-		Tekening t = new Tekening("");
-		Vorm rechthoek = new Rechthoek(new Punt(2,-2), 50, 50);
-		t.voegToe(rechthoek);
-	}
-
-	@Test (expected = DomainException.class)
-	public void Tekening_vorm_binnen_bounds_down(){
-		Tekening t = new Tekening("");
-		Vorm rechthoek = new Rechthoek(new Punt(2,2), 50, 500000);
-		t.voegToe(rechthoek);
 	}
 	
 	
