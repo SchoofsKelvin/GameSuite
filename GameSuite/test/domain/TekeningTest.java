c9e1f739a67c8a98b7648f740f4588fb5bf2db94
r0624401 <jens_wouters@live.be>
2016-05-18 14:27:49 +0200

    Merge TekeningTest

--- GameSuite/test/domain/TekeningTest.java
+++ GameSuite/test/domain/TekeningTest.java
@@ -4,29 +4,25 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import static org.junit.Before.*;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Before;
 import org.junit.Test;
 
+@SuppressWarnings("unused")
 public class TekeningTest {
-	private String naam ="jef";
+	private String naam = "jef";
 	private int MIN_X, MIN_Y, MAX_X, MAX_Y;
-	
-	
-	
+
+	private Tekening t;
+
 	@Before
-	public void setUp(){
-		/*Tekening t = new Tekening();
+	public void setUp() {
+		t = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		Vorm c = new Cirkel(new Punt(50, 50), 10);
 		t.voegToe(v);
-		t.voegToe(c);*/
+		t.voegToe(c);
 	}
-	
-	
+
 	@Test(expected = DomainException.class)
 	public void setNaam_gooit_exception_als_naam_null_is() {
 		Tekening t = new Tekening(null);
@@ -38,71 +34,75 @@ public class TekeningTest {
 		Tekening t = new Tekening(null);
 		t.voegToe(r);
 	}
-	
-	@Test(expected=DomainException.class)
-	public void als_de_index_negatief_is_gooit_exception(){
+
+	@Test(expected = DomainException.class)
+	public void als_de_index_negatief_is_gooit_exception() {
 		Tekening t = new Tekening(null);
 		t.getVorm(-1);
 	}
-	
+
 	@Test
-	public void je_moet_met_de_voeg_toe_methode_de_vorm_kunnen_toevoegen(){
-		Tekening t = new Tekening();
+	public void je_moet_met_de_voeg_toe_methode_de_vorm_kunnen_toevoegen() {
+		Tekening t = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		Vorm c = new Cirkel(new Punt(50, 50), 10);
 		t.voegToe(v);
 		t.voegToe(c);
 		System.out.println(t.getAantalVormen());
 	}
-	
-	@Test(expected=DomainException.class)
-	public void als_de_index_groter_dan_de_size_Van_lijst_gooit_exception(){
-		Tekening t = new Tekening();
+
+	@Test(expected = DomainException.class)
+	public void als_de_index_groter_dan_de_size_Van_lijst_gooit_exception() {
+		Tekening t = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		assertEquals(v, t.getVorm(2));
 	}
+
 	@Test
-	public void als_de_aantal_niet_gelijk_is_geeft_false_terug(){
-		Tekening t= new Tekening();
+	public void als_de_aantal_niet_gelijk_is_geeft_false_terug() {
+		Tekening t = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		Vorm c = new Cirkel(new Punt(50, 50), 10);
-			t.voegToe(v);
-			t.voegToe(c);
-			System.out.println(t.getAantalVormen());
+		t.voegToe(v);
+		t.voegToe(c);
+		System.out.println(t.getAantalVormen());
 		assertEquals(2, t.getAantalVormen());
 	}
+
 	@Test
-	public void als_de_vorm_niet_in_de_lijst_returnt_false(){
-		Tekening tekening = new Tekening();
+	public void als_de_vorm_niet_in_de_lijst_returnt_false() {
+		Tekening tekening = new Tekening("test");
 		Vorm v = new Rechthoek(new Punt(20, 80), 10, 10);
 		assertFalse(tekening.bevat(v));
 	}
+
 	@Test
-	public void get_vorm_by_index(){
-		Tekening t= new Tekening("tekeningNaam");
+	public void get_vorm_by_index() {
+		Tekening t = new Tekening("tekeningNaam");
 		Vorm v = new Rechthoek(new Punt(20, 10), 10, 10);
 		Vorm c = new Cirkel(new Punt(50, 50), 10);
 		t.voegToe(v);
 		t.voegToe(c);
 		assertEquals(v, t.getVorm(0));
 	}
-	
+
 	@Test
-	public void verwijderd_moet_vorm_verwijderen(){
-		Tekening tekening = new Tekening();
-		
-		Vorm driehoek = new Driehoek(new Punt(10,5), new Punt(5, 10), new Punt(3,8));
+	public void verwijderd_moet_vorm_verwijderen() {
+		Tekening tekening = new Tekening("test");
+
+		Vorm driehoek = new Driehoek(new Punt(10, 5), new Punt(5, 10), new Punt(3, 8));
 		Vorm rechthoek = new Rechthoek(new Punt(20, 10), 10, 10);
 		tekening.voegToe(driehoek);
 		tekening.voegToe(rechthoek);
-		
+
 		tekening.verwijder(rechthoek);
 		assertEquals(1, tekening.getAantalVormen());
 		assertTrue(tekening.bevat(driehoek));
 		assertFalse(tekening.bevat(rechthoek));
 	}
+
 	@Test
-	public void Tekening_equals(){
+	public void Tekening_equals() {
 		Tekening t = new Tekening("jef");
 		Tekening a = new Tekening("Bart");
 		Tekening b = new Tekening("john");
@@ -110,10 +110,5 @@ public class TekeningTest {
 		assertTrue(c.equals(a));
 		assertFalse(t.equals(b));
 	}
-	
-	
-	
-	
-	
 
 }
