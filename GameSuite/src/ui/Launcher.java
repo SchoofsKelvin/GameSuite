b09736f389f9ce87dc1ab4076c4eb4c322106e4d
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-05-18 14:28:04 +0200

    Call to System.exit() when escape is pushed on name dialog

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -9,6 +9,7 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
+		if(naam == null) System.exit(0);
 		Speler speler = new Speler(naam);
 		PictionaryUi ui = new PictionaryUi(speler);
 		ui.start();
