3b58dd83602409e7abc814d3728c09b5d04a224f
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-05-20 15:24:50 +0200

    Story 12: - HangManUi basis - HintLetter (af?) - HintWoord (af?)

---
+++ GameSuite/src/domain/HintWoord.java
@@ -0,0 +1,39 @@
+package domain;
+
+public class HintWoord {
+
+	private HintLetter[] letters;
+	private String woord;
+
+	public HintWoord(String woord) {
+		letters = new HintLetter[woord.length()];
+		for (int i = 0; i < woord.length(); i++) {
+			letters[i] = new HintLetter(woord.charAt(i));
+		}
+		this.woord = woord;
+	}
+
+	public boolean raad(char letter) {
+		boolean geraden = false;
+		for (HintLetter let : letters) {
+			geraden = geraden || let.raad(letter);
+		}
+		return geraden;
+	}
+
+	public boolean isGeraden() {
+		for (HintLetter let : letters) {
+			if (!let.isGeraden()) return false;
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return woord;
+	}
+	
+	public String getWoord() {
+		return woord;
+	}
+}
