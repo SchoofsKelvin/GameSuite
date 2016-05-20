d7c7fc4bf4935c4bed9af5c6f8ef9e6654b44202
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-05-20 17:09:29 +0200

    Merge Launcher

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -1,57 +1,18 @@
 package ui;
 
-import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
-import domain.Cirkel;
-import domain.Punt;
-import domain.Rechthoek;
 import domain.Speler;
-import domain.Tekening;
-import domain.Vorm;
 
 public class Launcher {
 
 	public static void main(String[] args) throws Exception {
 		String naam = JOptionPane.showInputDialog("Welkom! \nHoe heet je?");
-		
+
 		if (naam == null) {
 			System.exit(0);
 		}
 		Speler speler = new Speler(naam);
-<<<<<<< HEAD
-		PictionaryUi ui = new PictionaryUi(speler);
-		ui.start();
-		Tekening tekening = new Tekening("Test");
-
-		ArrayList<Vorm> list = (ArrayList<Vorm>) tekening.getVormen();
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i) instanceof Rechthoek) {
-				Rechthoek r = (Rechthoek) list.get(i);
-				tekening.voegToe(new Rechthoek(new Punt(r.getLinkerBovenhoek().getX(), r.getLinkerBovenhoek().getY()),
-						r.getBreedte(), r.getHoogte()));
-			}else if(list.get(i) instanceof Cirkel){
-				Cirkel c = (Cirkel) list.get(i);
-				tekening.voegToe(new Cirkel(new Punt(c.getMiddelpunt().getX(), c.getMiddelpunt().getY()), c.getRadius()));
-			}
-		}
-		ui.toonTekening(speler.getNaam(), tekening);
-		/*
-		 * tekening.voegToe(new Rechthoek(new Punt(10, 10), 70, 50));
-		 * tekening.voegToe(new Cirkel(new Punt(200, 150), 100));
-		 * tekening.voegToe(new Driehoek(new Punt(5, 10), new Punt(100, 150),
-		 * new Punt(300, 50))); tekening.voegToe(new LijnStuk(new Punt(100,
-		 * 200), new Punt(200, 300)));
-		 */
-
-		// GameMainWindow window = new GameMainWindow("Pictionary", tekening);
-		// window.setVisible(true);
-		// window.teken();
-=======
-		//PictionaryUi ui = new PictionaryUi(speler);
-		//ui.start();
->>>>>>> branch 'master' of https://github.com/SchoofsKelvin/GameSuite.git
 		Class<? extends Spel> spel = getSpel();
 		spel.getConstructor(Speler.class).newInstance(speler).start();
 	}
@@ -64,7 +25,8 @@ public class Launcher {
 		for (int i = 0; i < games.length; i++) {
 			names[i] = (String) games[i].getMethod("getSpelNaam").invoke(null);
 		}
-		String spel = (String) JOptionPane.showInputDialog(null, "Welk spel wil je spelen?", "input",
+		String spel = (String) JOptionPane.showInputDialog(null,
+				"Welk spel wil je spelen?", "input",
 				JOptionPane.INFORMATION_MESSAGE, null, names, null);
 		for (int i = 0; i < names.length; i++) {
 			if (names[i].equals(spel))
