900351e511ed66f5e47c89937b2182963854d0a0
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-05-20 15:59:05 +0200

    Fix imports

--- GameSuite/src/ui/Launcher.java
+++ GameSuite/src/ui/Launcher.java
@@ -1,9 +1,8 @@
 package ui;
 
-import domain.Punt;
-import domain.Rechthoek;
+import javax.swing.JOptionPane;
+
 import domain.Speler;
-import domain.Tekening;
 
 public class Launcher {
 
@@ -13,16 +12,16 @@ public class Launcher {
 			System.exit(0);
 		}
 		Speler speler = new Speler(naam);
-		/*PictionaryUi ui = new PictionaryUi(speler);
-		ui.start();
-		Tekening tekening = new Tekening("Test");
-		tekening.voegToe(new Rechthoek(new Punt(10, 10), 70, 50));
-		tekening.voegToe(new Cirkel(new Punt(200, 150), 100));
-		tekening.voegToe(new Driehoek(new Punt(5, 10), new Punt(100, 150), new Punt(300, 50)));
-		tekening.voegToe(new LijnStuk(new Punt(100, 200), new Punt(200, 300)));
-		GameMainWindow window = new GameMainWindow("Pictionary", tekening);
-		window.setVisible(true);
-		window.teken();*/
+		/*
+		 * PictionaryUi ui = new PictionaryUi(speler); ui.start(); Tekening
+		 * tekening = new Tekening("Test"); tekening.voegToe(new Rechthoek(new
+		 * Punt(10, 10), 70, 50)); tekening.voegToe(new Cirkel(new Punt(200,
+		 * 150), 100)); tekening.voegToe(new Driehoek(new Punt(5, 10), new
+		 * Punt(100, 150), new Punt(300, 50))); tekening.voegToe(new
+		 * LijnStuk(new Punt(100, 200), new Punt(200, 300))); GameMainWindow
+		 * window = new GameMainWindow("Pictionary", tekening);
+		 * window.setVisible(true); window.teken();
+		 */
 		Class<? extends Spel> spel = getSpel();
 		spel.getConstructor(Speler.class).newInstance(speler).start();
 	}
@@ -31,16 +30,14 @@ public class Launcher {
 	private static Class<? extends Spel> getSpel() throws Exception {
 		Class[] games = { PictionaryUi.class, HangManUi.class };
 		String[] names = new String[games.length];
-		for (int i=0; i<games.length; i++) {
+		for (int i = 0; i < games.length; i++) {
 			names[i] = (String) games[i].getMethod("getSpelNaam").invoke(null);
 		}
 		String spel = (String) JOptionPane.showInputDialog(null,
 				"Welk spel wil je spelen?", "input",
 				JOptionPane.INFORMATION_MESSAGE, null, names, null);
-		for (int i=0; i<names.length; i++) {
-			if (names[i].equals(spel)) {
-				return games[i];
-			}
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(spel)) return games[i];
 		}
 		return null;
 	}
