b8864c58cd5aa7acb7d628f16958294f9c0f1ead
ThomasWouters <thomaswouters@outlook.com>
2016-05-20 17:24:45 +0200

    WoordenLezer

--- GameSuite/src/db/WoordenLezer.java
+++ GameSuite/src/db/WoordenLezer.java
@@ -1,9 +1,37 @@
 package db;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import domain.WoordenLijst;
+
 public class WoordenLezer {
 
+	private File file;
+	private WoordenLijst woordenLijst;
+	private String[] woorden;
+	
 	public WoordenLezer(String bestandsNaam) {
+		file = new File(bestandsNaam);
+	}
+	
+	public WoordenLijst lees() {
+		try {
+			Scanner scanner = new Scanner(file);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				woorden = line.split("\n");
+			}
+			
+			for (int i = 0; i<woorden.length; i++) {
+				woordenLijst.voegToe(woorden[i]);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 		
+		return woordenLijst;
 	}
 	
 	
