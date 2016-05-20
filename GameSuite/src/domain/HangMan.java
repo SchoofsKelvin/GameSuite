db357a61fb4960f0096bcd7cc74b9283255cde0d
Razacx <levi.vandenbempt.sjcm@gmail.com>
2016-05-20 16:44:41 +0200

    Added class + some methods

---
+++ GameSuite/src/domain/HangMan.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class HangMan {
+    public String getHint() {
+        return "";
+    }
+
+    public Tekening getSpeler() {
+        return null;
+    }
+
+    public Tekening getTekening() {
+        return null;
+    }
+}
